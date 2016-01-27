4f7afc1b720f78f66753b2e9408794fdec0a7ec3
David Sok <sokd88@gmail.com>
2016-01-26 19:16:56 -0500

    correctly ranks

--- js/main.js
+++ js/main.js
@@ -1,43 +1,54 @@
-'use strict';
-$(document).ready(function() {
-  var scoreboard;
-  var rankings = [];
-  var displayScoreboard = function(rankings) {
-    var rankPosition = 1;
-    $('#rankings').html('')
-    for (var i = 0; i < rankings.length; i++) {
-      if (i > 0 && rankings[i-1] == rankings[i]) {
-        rankPosition += 1
-      }
-      $('#rankings').append('<li>' + rankPosition + '. ' +rankings[i].name + ', ' + rankings[i].score + ' pts</li>')
+var scoreboard;
+var rankings = [];
+var displayScoreboard = function(rankings) {
+  var rankPosition = 1;
+  var tieScore = 0
+  $('table tbody').empty()
+  $('#rankings').empty()
+  for (var i = 0; i < rankings.length; i++) {
+    if (i > 0 && rankings[i-1].score !== rankings[i].score) {
+      rankPosition = i + 1;
+      tieScore = 0
+    } else {
+      tieScore += 1
     }
-  }
-  var sortRankings = function(rankings) {
-    rankings.sort(function(a, b) {
-      return b.score-a.score;
-    });
-  }
+    $('table').append('<tr><td>' + rankPosition + '</td><td> ' +rankings[i].name + '</td><td> ' + rankings[i].score + ' pts</td></tr>');
 
-  $('#add').on('click', function() {
-    var playerName = $('#eq-name').val();
-    var playerScore = $('#eq-score').val();
+    $('#rankings').append('<li>' + rankPosition + '. ' +rankings[i].name + ', ' + rankings[i].score + ' pts </li>');
+  };
+}
+var sortRankings = function(rankings) {
+  rankings.sort(function(a, b) {
+    return b.score-a.score;
+  });
+};
+$('#add').on('click', function() {
+  var playerInput = $('#eq-player-input').val();
+  var parsedPlayerInput = playerInput.split(',');
+  var playerName = parsedPlayerInput[0]
+  var playerScore = parseInt(parsedPlayerInput[1], 10)
 
-    if (playerName.length > 0 && playerScore.length > 0) {
+  if (playerName.length > 0 && playerScore >= 0) {
+    var existingPlayer = false;
+    for (var i = 0; i < rankings.length && !existingPlayer; i++) {
+      if (playerName == rankings[i].name) {
+        rankings[i].score += playerScore
+        existingPlayer = true;
+      }
+    };
+    if (!existingPlayer) {
       var playerStats = {name: playerName, score: playerScore};
       rankings.push(playerStats);
-      sortRankings(rankings);
-      displayScoreboard(rankings);
-      $('#eq-name').val('');
-      $('#eq-score').val('');
-    } else {
-      alert("Please enter a name and a score");
     }
-  });
-
-  $('#clear').on('click', function() {
-    $('#rankings').html('');
-    rankings = [];
-  });
-
-
+    sortRankings(rankings);
+    displayScoreboard(rankings);
+    $('#eq-player-input').val('');
+  } else {
+    alert("Please enter a name and a score in the format: John Doe, 15");
+  }
+});
+$('#clear').on('click', function() {
+  $('table tbody').html('');
+  $('#rankings').html('');
+  rankings = [];
 });
